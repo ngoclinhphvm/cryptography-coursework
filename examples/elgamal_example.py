67e03e63488a0c99936ad2c53fa633615690f0f4
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-10-27 23:38:26 +0700

    docs: add usage examples for elgamal

---
+++ examples/elgamal_example.py
@@ -0,0 +1,26 @@
+from algorithms.elgamal.elgamal import encrypt, decrypt, generate_keypair
+
+def main():
+    # Key generation
+    public_key, private_key = generate_keypair(128)
+    print(f"Public key (p, alpha, beta): {public_key}")
+    print(f"Private key (p, a): {private_key}")
+    
+    # Message
+    message = input('Enter message: ')
+    encoded_message = int.from_bytes(message.encode('utf-8'), 'big')
+    print(f"\nOriginal message: {encoded_message}")
+    
+    # Encryption
+    encrypted = encrypt(encoded_message, public_key)
+    print(f"Encrypted: {encrypted}")
+    
+    # Decryption
+    decrypted = decrypt(encrypted, private_key)
+    print(f"Decrypted: {decrypted}")
+    
+    # Check if message is successfully recovered
+    print(f"Message successfully recovered: {encoded_message == decrypted}")
+
+if __name__ == "__main__":
+    main()
